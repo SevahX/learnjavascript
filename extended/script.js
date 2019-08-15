10b48b7c9713fa5a28168ca2d316c081e4934907
sevahx <sevahx@gmail.com>
2019-08-15 23:13:29 +0200

    Last

--- extended/script.js
+++ extended/script.js
@@ -3,14 +3,14 @@
 let a = {
     name: 'Misha',
     order: {
-        price: 20
+        price: 1
     },
 };
 
 let b = {
     name: 'Misha',
     order: {
-        price: 20,
+        price: 1,
     }
 };
 
@@ -22,10 +22,10 @@ function deepEqual(a, b) {
     if (Object.keys(a).length != Object.keys(b).length) return false;
 
     for (let key in a) {
-        console.log(`a key=${key}:${a[key]} - ${typeof a[key]}`);
-        console.log(`b key=${key}:${b[key]} - ${typeof a[key]}`);
+        // console.log(`a key=${key}:${a[key]} - ${typeof a[key]}`);
+        // console.log(`b key=${key}:${b[key]} - ${typeof a[key]}`);
 
-        if (typeof a[key] === 'object') {
+        if (a[key] !== null && typeof a[key] === 'object') {
             if (typeof b[key] !== 'object') return false;
             if (!deepEqual(a[key], b[key])) return false;
         } else if (a[key] !== b[key]) {
